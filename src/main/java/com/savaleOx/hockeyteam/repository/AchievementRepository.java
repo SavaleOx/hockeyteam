6341a89e5e503138413bfac237747801f0889f3a
SavaleOx <alex312sovu@gmail.com>
2026-03-14 21:18:44 +0300

    ..

--- src/main/java/com/savaleOx/hockeyteam/repository/AchievementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.savaleox.hockeyteam.repository;
-
-import com.savaleox.hockeyteam.model.entity.Achievement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AchievementRepository extends JpaRepository<Achievement, Long> {
-    Achievement findByName(String name);
-}
--- src/main/java/com/savaleOx/hockeyteam/repository/PositionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.savaleox.hockeyteam.repository;
-
-import com.savaleox.hockeyteam.model.entity.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PositionRepository extends JpaRepository<Position, Long> {
-    Position findByName(String name);
-}
