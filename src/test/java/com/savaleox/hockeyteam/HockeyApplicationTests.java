17e1812899de75d98f7a6f4c7660fb24df04c6ab
SavaleOx <alex312sovu@gmail.com>
2026-05-01 22:45:22 +0300

    test

--- src/test/java/com/savaleox/hockeyteam/HockeyApplicationTests.java
+++ src/test/java/com/savaleox/hockeyteam/HockeyApplicationTests.java
@@ -3,14 +3,23 @@ package com.savaleox.hockeyteam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = HockeyApplicationTests.TestApplication.class)
 class HockeyApplicationTests {
 
     @SpringBootConfiguration
-    @EnableAutoConfiguration
+    @EnableAutoConfiguration(exclude = {
+            DataSourceAutoConfiguration.class,
+            HibernateJpaAutoConfiguration.class
+    })
     static class TestApplication {
     }
 
+    @Test
+    void contextLoads() {
+
+    }
 }
